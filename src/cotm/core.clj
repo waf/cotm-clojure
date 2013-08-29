1e258aa2de6c5d2fc2e396d9a8e02eab4f23ae49
Will Fuqua <wafuqua@gmail.com>
2013-08-29 00:46:21 -0400

    add 'move to center'

--- src/cotm/core.clj
+++ src/cotm/core.clj
@@ -2,29 +2,61 @@
   (require [lanterna.screen :as s]
            [clojure.data.json :as json]))
 
-(defn draw-random [scr text]
-  (let [[x y] (map #(rand-int %) (s/get-size scr))]
-    (s/clear scr)
-    (s/put-string scr x y text)
-    (s/redraw scr)))
-  
-(defn get-commands [path]
-  (json/read-str (slurp path)))
+(defn pause [time]
+  (Thread/sleep time))
 
 (defn get-active-commands [cmds]
-  (keys (filter second cmds)))
+  ; cmds is a map, iterate over [command, isActive] pairs
+  (keys (filter second cmds))) 
 
-(defn cycle-commands [scr cmds]
-  (dotimes [n 15] 
+(defn draw-text [scr x y text]
+  (s/clear scr)
+  (s/put-string scr x y text)
+  (s/redraw scr))
+
+; draw 'text' to the screen at a random position
+(defn draw-random [scr text]
+  ; get a random point on the screen, making sure the text won't run off the end
+  (let [[x y] (map #(rand-int (- % (count text))) 
+                   (s/get-size scr))]
+    (draw-text scr x y text)
+    [x y]))
+
+; flash a sequence of random commands on the screen in various positions
+(defn animate-random [scr cmds]
+  (dotimes [n (+ 15 (rand-int 5))] 
     (draw-random scr (rand-nth cmds))
-    (Thread/sleep 400)))
+    (pause 400)))
+
+; given two [x y] points ('to' and 'from'), return an [x y] point that is
+; one coordinate closer to the 'to' point than the 'from' point
+; argument order is reverse from expected for partial application convenience
+(defn delta-move [to from]
+  ; abuse the fact that compare returns -1, 0, 1 for lt, eq, gt
+  (map (fn [p1 p2] (+ p1 (compare p2 p1))) from to))
+
+; given from and to points, generate a list of points between 'from' and 'to'
+(defn generate-path [from to]
+  (take-while (partial not= to)
+              (iterate (partial delta-move to) from)))
+
+; place the text on the screen in a random position, then 
+; animate it to the center of the screen
+(defn animate-random-to-center [scr text]
+  (let [[sx sy] (s/get-size scr)
+        cx (int (/ (- sx (count text)) 2))
+        cy (int (/ sy 2))]
+  (doseq [[x y] (generate-path (draw-random scr text) [cx cy])]
+    (draw-text scr x y text)
+    (pause 100))
+  (draw-text scr cx cy text)))
 
 (defn -main [& args]
-  (let [scr (s/get-screen :text)
-        cmds (get-commands "resources/commands.json")
+  (let [scr (s/get-screen)
+        cmds (json/read-str (slurp "resources/commands.json"))
         actives (get-active-commands cmds)
         chosen (rand-nth actives)]
     (s/in-screen scr
-                 (cycle-commands scr (remove #{chosen} actives))
-                 (draw-random scr chosen)
+                 (animate-random scr (remove #{chosen} actives))
+                 (animate-random-to-center scr chosen)
                  (s/get-key-blocking scr))))
